c8459ae423ca0783d17b277cfbbf1c1198d5d39b
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-08-18 07:35:09 -0300

    Redux config

--- src/components/MyListas.js
+++ src/components/MyListas.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+//importo hooks para usar y manipular el state
+import { useSelector, useDispatch } from "react-redux";
+//importo las funciones de mi slice
+import { counterActions } from "../Slice";
 
-export default function MyListas(props) {  
+export default function MyListas(props) {
+  //para obtener el state y mostrarlo (counterdata lo definí en el store)
+  const count = useSelector((state) => state.counterdata.value);
+  //para luego utilizar las funciones del slice
+  const dispatch = useDispatch();
   return (
     <div>
-      <h2>My lists</h2> 
+      <h2>My lists</h2>
       {/* mylista */}
-      <div>
-        {/*  */}
-      </div>
+      <div>{count}</div>
+      <button onClick={() => dispatch(counterActions.increment())}>
+        Increment
+      </button>
     </div>
   );
-} 
+}
